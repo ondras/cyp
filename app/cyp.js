2957ded7d832c14ddac09362013c1e2cd02e39d7
Ondrej Zara <ondrej.zara@gmail.com>
2020-03-28 18:31:28 +0100

    cleanup

--- app/cyp.js
+++ app/cyp.js
@@ -808,29 +808,21 @@ class Component extends HTMLElement {
 }
 
 class Menu extends Component {
-	constructor() {
-		super();
-
-		this._tabs = Array.from(this.querySelectorAll("[data-for]"));
-		this._tabs.forEach(tab => {
-			tab.addEventListener("click", _ => this._activate(tab.dataset.for));
-		});
-	}
-
 	_onAppLoad() {
+		/** @type HTMLElement[] */
+		this._tabs = Array.from(this.querySelectorAll("[data-for]"));
+
+		this._tabs.forEach(tab => {
+			tab.addEventListener("click", _ => this._app.setAttribute("component", tab.dataset.for));
+		});
+
 		this._app.addEventListener("queue-length-change", e => {
 			this.querySelector(".queue-length").textContent = `(${e.detail})`;
 		});
 
 	}
-
-	async _activate(component) {
-		const app = await this._app;
-		app.setAttribute("component", component);
-	}
-
 	_onComponentChange(component) {
-		this._tabs.forEach(/** @param {HTMLElement} tab */ tab => {
+		this._tabs.forEach( tab => {
 			tab.classList.toggle("active", tab.dataset.for == component);
 		});
 	}
